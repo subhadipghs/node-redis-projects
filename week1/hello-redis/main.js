45706fe9fb9a49d1b8778923c316f70185305d35
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-03-08 09:40:28 +0530

    redis events

--- week1/hello-redis/main.js
+++ week1/hello-redis/main.js
@@ -12,15 +12,26 @@ async function main(key = 'hello', value = 'redis') {
   const ok = 'OK'
   await client.connect()
   const k = await client.set(key, value)
-  console.log(k)
   if (k == ok) {
     const reply = await client.get(key)
     await client.quit()
+    console.log(reply)
     return reply
   }
   return null
 }
 
+client.on('ready', () => {
+  console.log('client is ready')
+})
+
+client.on('end', () => {
+  console.log('disconnected')
+})
+
+client.on('reconnecting', () => {
+  console.log('oops! reconnecting...')
+})
 
 module.exports = {
   main
