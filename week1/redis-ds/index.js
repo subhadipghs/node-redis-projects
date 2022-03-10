a1408add08cf980220782d94393e9ad0803b7bd7
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-03-10 10:57:22 +0530

    Modify folder structure

---
+++ week1/redis-ds/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const redis = require('redis')
+const tap = require('tap')
+
+
+
+tap.test('redis data structures', t => {
+  t.test('redis hash data structure', t => {
+    t.equal(1, 1)
+    t.end()
+  })
+  t.end()
+})
