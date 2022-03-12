617fe541b8e11b3474a19e57e94f153c88c93fb5
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-03-12 10:56:08 +0530

    Added lua script

---
+++ week2/lua-scripts/update.lua
@@ -0,0 +1,10 @@
+local key = KEYS[1]
+local new = ARGV[1]
+local current = redis.call('GET', key)
+
+if (current == false) or (tonumber(new) < tonumber(current)) then
+  redis.call('SET', key, new)
+  return 1
+else
+  return 0
+end;
