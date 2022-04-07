a5518a3837e01bd31cb33ef8625e5147c0645e19
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-04-07 10:18:36 +0530

    added: xrange for streams

--- week3/streams/index.js
+++ week3/streams/index.js
@@ -12,15 +12,34 @@ tap.test("redis streams", (test) => {
     try {
       const key = "test:stream:2";
       const entry = ["id", 1, "email", "test@gmail.com"];
-      await client.xAdd(key, '*', "MAXLEN", "~", 2, ...entry);
+      await client.xAdd(key, "*", "MAXLEN", "~", 2, ...entry);
       const len = await client.xLen(key);
       assert.equal(len, 1);
     } catch (e) {
       console.error(e);
-      assert.fail('should add an entry to the stream')
+      assert.fail("should add an entry to the stream");
     } finally {
       assert.end();
     }
+  });
+  test.test("should retrieve the stream entries using xrange from oldest to newest", async (assert) => {
+    const key = "test:stream:3";
+    const entry = [
+      {"id": 2, "email": "test2@mail.com"},
+      {"id": 1, "email": "test2@mail.com"},
+    ];
+    for (let i = 0; i < entry.length; i++) {
+      await client.xAdd(key, "*", entry[i]);
+    }
+    const len = await client.xLen(key);
+    assert.equal(len, entry.length);
+    const results = await client.xRange(key, "-", "+");
+    results.forEach((result, i) => {
+      assert.ok(result.id)
+      assert.equal(result.message.id, entry[i].id.toString())
+      assert.equal(result.message.email, entry[i].email)
+    })
+    assert.end();
   });
   test.end();
 });
